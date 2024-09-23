f9c10d20312e9431e7803ed6a0ba18eedf19f0e0
Pavan-0228 <pavanrasal4@gmail.com>
2024-09-24 00:16:30 +0530

    .

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
+import path from "path";
+
 const app = express({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
@@ -19,8 +21,23 @@ app.use("/api/v1/auth", userRouter);
 
 app.use("/api/v1/posts", postRouter);
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
+
+const _dirname = path.dirname("")
+const buildPath = path.join(_dirname  , "../frontend/dist");
+
+app.use(express.static(buildPath))
+
+app.get("/*", function(req, res){
+
+    res.sendFile(
+        path.join(__dirname, "../frontend/dist/index.html"),
+        function (err) {
+          if (err) {
+            res.status(500).send(err);
+          }
+        }
+    );
+})
+
 
 export { app };
